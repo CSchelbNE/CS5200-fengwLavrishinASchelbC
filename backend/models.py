a2c1d76942e2ca9f2b565a9b081cfde13d5f76a2
Cschelb <85119920+Cschelb@users.noreply.github.com>
2022-11-01 13:29:02 -0400

    modified the index for name in models to be unique

--- backend/models.py
+++ backend/models.py
@@ -6,7 +6,7 @@ from sqlalchemy.orm import relationship
 class User(Base):
     __tablename__ = "users"
     user_id = Column(Integer, Sequence("user_id"), primary_key=True, index=True)
-    name = Column(String(50), nullable=False)
+    name = Column(String(50), unique=True, nullable=False)
     address = Column(String(100), nullable=False)
     email = Column(String(50), unique=True, index=True)
     password = Column(String(50), nullable=False)
