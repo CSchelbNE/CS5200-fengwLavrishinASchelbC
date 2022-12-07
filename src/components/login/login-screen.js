a62d498a3c20e483893e89ab6b2fedb84ec2d406
mac <mac@macs-Air.cable.rcn.com>
2022-12-07 17:33:02 -0500

    fixed login string

--- src/components/login/login-screen.js
+++ src/components/login/login-screen.js
@@ -9,7 +9,7 @@ import {addUser} from "../../redux/reducers/user";
 import {changeFocus} from "../../redux/reducers/ticket-reducer";
 
 const LoginScreen = () => {
-    const URL_STRING = "https://cs5200-backend.herokuapp.com/login";
+    const URL_STRING = "https://cs5200-backend.herokuapp.com/users/login";
     const [userName, setUsername] = React.useState("");
     const [password, setPassword] = React.useState("");
     const dispatch = useDispatch();
