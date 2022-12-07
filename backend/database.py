29ef8e367e2bdcf6a3decf086807a203572f2744
mac <mac@macs-Air.cable.rcn.com>
2022-12-07 14:44:16 -0500

    testing localhost

--- backend/database.py
+++ backend/database.py
@@ -7,20 +7,27 @@ REMOTE_DATABASE_URL = "mysql+pymysql://admin:cs5200finalproject@database-1.c5mdh
                       "/ticket_system"
 
 
-username = "root"
-password = "F1shg0d12345!"
+first_load = True
+if first_load:
+    username = input("Username: ").strip("")
+    password = input("Password: ").strip("")
 # NO SPECIAL CHARACTERS IN THE PASSWORD THAT MIGHT CONFLICT WITH THE LOCALHOST STRING PARSING (@ IS BAD)
-LOCAL_HOST_URL = "mysql+pymysql://" + username + ":" + \
-                 password + "@localhost"
+
 
 REMOTE_DATABASE_URL = "mysql+pymysql://admin:cs5200finalproject@database-1.c5mdh4lrufto.us-east-2.rds.amazonaws.com" \
                       "/ticket_system"
 
+LOCAL_HOST_URL = "mysql+pymysql://" + username + ":" + \
+                 password + "@localhost"
 # aws remote server password is cs5200finalproject
 engine = sql.create_engine(LOCAL_HOST_URL,
                            connect_args=dict(host='localhost', port=3306))
 while True:
     try:
+        LOCAL_HOST_URL = "mysql+pymysql://" + username + ":" + \
+                         password + "@localhost"
+        engine = sql.create_engine(LOCAL_HOST_URL,
+                                   connect_args=dict(host='localhost', port=3306))
         conn = engine.connect()
         Base = declarative_base()
         session = sessionmaker(autocommit=False, autoflush=False, bind=engine)
@@ -30,6 +37,10 @@ while True:
     except Exception as error:
         print("Connection failed")
         print("Error:" + error.__str__())
+        time.sleep(1)
+        username = input("Username: ")
+        password = input("Password: ")
+
 
 
 def get_db():
@@ -38,4 +49,17 @@ def get_db():
         yield conn
     finally:
         db.close()
+
+
+# aws remote server password is cs5200finalproject
+# try:
+#     engine = sql.create_engine(LOCAL_HOST_URL,
+#                                connect_args=dict(host='localhost', port=3306))
+#     Base = declarative_base()
+#     conn = engine.connect()
+#     conn.execute("USE ticket_system")
+#     print("Database connection was successful")
+# except Exception as error:
+#     print("Connection failed")
+#     print("Error:" + error.__str__())
 
